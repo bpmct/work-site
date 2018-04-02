b196deedb7a1713fe266d24978fe5edabd0cc372
Ben <me@bpmct.net>
2018-04-02 08:47:04 -0700

    idk changed something small and stupid

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ options = {
     onconfig: function (config, next) {
         /*
          * Add any additional config setup or overrides here. `config` is an initialized
-         * `confit` (https://github.com/krakenjs/confit/) configuration object.
+         * `confit` (https://github.com/krakenjs/confit/) configuration object.1
          */
         db.config(config.get('databaseConfig'));
         next(null, config);
