bef12d9422923ef1750deb09b8055bf18c63501c
Ben <me@bpmct.net>
2018-04-09 06:08:44 -0700

    new var book

--- controllers/books.js
+++ controllers/books.js
@@ -19,6 +19,8 @@ module.exports = function (router) {
                 book: book
             };
 
+            var book2 = hellobook2;
+
             res.render('books/details', model);
         });
     });
